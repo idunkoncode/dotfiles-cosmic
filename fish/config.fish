572dd3440d9d488b685edad9dd8eed58b6d528bc
idunkoncode <idunkoncode@proton.me>
2026-03-02 07:28:23 -0800

    initial dotfiles commit

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+oh-my-posh init fish --config ~/.cache/oh-my-posh/themes/gruvbox.omp.json | source    # smth smth
+#end
